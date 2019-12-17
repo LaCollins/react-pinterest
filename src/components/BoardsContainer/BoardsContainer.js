9419c3670656f5c7574a333c6b4a1a77830c7937
Laura Collins <laura.e.collins1@gmail.com>
2019-12-16 20:41:25 -0600

    boards are adding

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import authData from '../../helpers/data/authData';
 import boardData from '../../helpers/data/boardData';
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 class BoardsContainer extends React.Component {
   static propTypes = {
@@ -15,6 +16,10 @@ class BoardsContainer extends React.Component {
   }
 
   componentDidMount() {
+    this.getBoards();
+  }
+
+  getBoards = () => {
     boardData.getBoardsByUid(authData.getUid())
       .then((boards) => {
         this.setState({ boards });
@@ -22,8 +27,20 @@ class BoardsContainer extends React.Component {
       .catch((errorFromBoardsContainer) => console.error({ errorFromBoardsContainer }));
   }
 
+  addBoard = (newBoard) => {
+    boardData.saveBoard(newBoard)
+      .then(() => {
+        this.getBoards();
+      })
+      .catch((error) => console.error(error));
+  }
+
   render() {
-    return (<div className="row m-2 d-flex justify-content-around">{this.state.boards.map((board) => <Board key={board.id} board={board} setSingleBoard={this.props.setSingleBoard} />)}</div>);
+    return (
+    <div>
+    <BoardForm addBoard={this.addBoard}/>
+    <div className="row m-2 d-flex justify-content-around">{this.state.boards.map((board) => <Board key={board.id} board={board} setSingleBoard={this.props.setSingleBoard} />)}</div>
+    </div>);
   }
 }
 
